7fa6c768f067aad7534742f56a6a335cd75fea20
Charles <charlesheflin@gmail.com>
2026-03-30 15:19:02 -0400

    fix: Triple Instagram button image size

--- src/components/DoctorCredibility.tsx
+++ src/components/DoctorCredibility.tsx
@@ -30,7 +30,7 @@ const DoctorCredibility = () => {
                                 <img
                                     src="/bonuses/new-insta-button.jpg"
                                     alt="Follow Dr. Schutza on Instagram"
-                                    className="h-10 w-auto hover:opacity-80 transition-opacity"
+                                    className="h-[120px] w-auto hover:opacity-80 transition-opacity"
                                     loading="lazy"
                                 />
                             </a>
