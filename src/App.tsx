70db8bdd0fc6779cb2349d1293f54f7d9c43e3d2
Charles <charlesheflin@gmail.com>
2026-05-24 18:38:51 -0400

    Revert "feat: add /newindex and /newwalkthrough staged routes"
    This reverts commit 59070a8d209db86f5c313722f6cdcb50ad1fd281.

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ import NotFound from "./pages/NotFound.tsx";
 import RefundPolicy from "./pages/RefundPolicy.tsx";
 import EULA from "./pages/EULA.tsx";
 import Walkthrough from "./pages/Walkthrough";
-import NewIndex from "./pages/NewIndex.tsx";
-import NewWalkthrough from "./pages/NewWalkthrough.tsx";
 import CookieConsent from "./components/CookieConsent.tsx";
 
 const queryClient = new QueryClient();
@@ -47,12 +45,10 @@ const App = () => (
            <Route path="/thank-you" element={<ThankYou />} />
             <Route path="/choose-your-plan" element={<ChooseYourPlan />} />
             <Route path="/refund-policy" element={<RefundPolicy />} />
-             <Route path="/eula" element={<EULA />} />
-              <Route path="/walkthrough" element={<Walkthrough />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="/newindex" element={<NewIndex />} />
-              <Route path="/newwalkthrough" element={<NewWalkthrough />} />
-             <Route path="*" element={<NotFound />} />
+            <Route path="/eula" element={<EULA />} />
+             <Route path="/walkthrough" element={<Walkthrough />} />
+             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
          </Routes>
       </BrowserRouter>
     </TooltipProvider>
