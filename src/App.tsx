462a7460f17f403fca51ba7669d6419f7f4e0b11
Charles <charlesheflin@gmail.com>
2026-05-28 10:19:16 -0400

    add flare-up-guide landing page at /lp/flare-up-guide with styled Aweber form

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import Index_archive from "./pages/Index_archive_2026_05_25";
 import Walkthrough_archive from "./pages/Walkthrough_archive_2026_05_25";
 import CookieConsent from "./components/CookieConsent.tsx";
 import EmailConfirmation from "./pages/EmailConfirmation";
+import FlareUpGuide from "./pages/FlareUpGuide";
 
 const queryClient = new QueryClient();
 
@@ -58,6 +59,7 @@ const App = () => (
                   <Route path="/oldindex" element={<Index_archive />} />
                   <Route path="/oldwalkthrough" element={<Walkthrough_archive />} />
                 <Route path="/email-confirmation" element={<EmailConfirmation />} />
+                <Route path="/lp/flare-up-guide" element={<FlareUpGuide />} />
                 <Route path="*" element={<NotFound />} />
          </Routes>
       </BrowserRouter>
