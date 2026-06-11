6da61fcb599e45295566c90a55f7e7b6e91d7a4f
Charles <charlesheflin@gmail.com>
2026-06-11 09:20:53 -0400

    App: add /results route for email query param lookup

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ const App = () => (
                    <Route path="/start/new" element={<StartNew />} />
                    <Route path="/checkout" element={<Checkout />} />
                    <Route path="/results/:userId" element={<Results />} />
-                   <Route path="/assessment-results" element={<AssessmentResults />} />
+                   <Route path="/results" element={<Results />} />                   <Route path="/assessment-results" element={<AssessmentResults />} />
                     <Route path="*" element={<NotFound />} />
          </Routes>
       </BrowserRouter>
